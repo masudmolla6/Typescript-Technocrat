948d3dd816aeb3de3730e8bbea8648c1d5819bf2
Masud Molla <masudmolla2937@gmail.com>
2025-08-29 18:57:22 +0600

    added non premitive data type

---
+++ Module 01/dist/1.4.js
@@ -0,0 +1,12 @@
+// Basic Data Type
+// string
+let myName = "Masud Rana";
+// number
+let myRoll = 67;
+// boolean
+let isAdmin = true;
+// undefined
+let myWork = undefined;
+// null
+let myStatus = null;
+export {};
--- Module 01/src/1.4.ts
+++ Module 01/src/1.4.ts
@@ -1,11 +1,33 @@
-// string
+// Basic Premetive Data Type
 
+// string
 let myName:string="Masud Rana";
 
 // number
-
 let myRoll=67;
 
+// boolean
 let isAdmin:boolean=true;
 
+// undefined
+let myWork:undefined=undefined;
 
+// null
+let myStatus:null=null;
+
+//any
+
+let y;
+
+// Basic Non-Premetive Data Type 
+
+// Array
+
+let myFriend:string[]=["masud", "Rana"];
+
+let rolls:number[]=[1,2,4,6];
+
+// tuple->array->order->type of values
+let coordinate:[number, number]=[1,5];
+
+let ageNumber:[number, string]=[27, "Masud"];
