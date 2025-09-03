61d09520e052e95109c3829f2302ae691cc48f46
Masud Molla <masudmolla2937@gmail.com>
2025-09-03 22:51:45 +0600

    Complete Module 2

---
+++ Module 02/dist/2.10.js
@@ -0,0 +1,13 @@
+{
+    // Mapped Type
+    const arrayOfNumbers = [1, 2, 3, 4];
+    const arrayOfString = arrayOfNumbers.map((num) => {
+        return num;
+    });
+    const area1 = {
+        height: "100",
+        width: 50,
+    };
+    // 
+}
+export {};
---
+++ Module 02/dist/2.11.js
@@ -0,0 +1,18 @@
+{
+    const readOnlyPerson = {
+        name: "Masud Rana",
+        age: 26,
+        email: "mr@gmail.com",
+        contactNo: 56345,
+    };
+    readOnlyPerson.name = "Masu";
+    const obj1 = {
+        a: "sdf",
+        b: "sdfdf",
+        c: "dfrdf",
+        d: "rer",
+    };
+    const emptyObj = {};
+    // 
+}
+export {};
