78a575a796800ec1c2abd8473d0d921760faab90
Masud Molla <masudmolla2937@gmail.com>
2025-09-10 11:36:55 +0600

    Encapsulation in OOP

---
+++ Module 03/src/3.10.ts
@@ -0,0 +1,43 @@
+{
+  //
+  // Encapsulation in OOP
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    private _balance:number; //(private propery user er khatre covenseion holo _ (underscore use kora))
+    // protected _balance: number;
+
+    constructor(id: number, name: string, _balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = _balance;
+    }
+
+    public addAmount(amout: number) {
+      this._balance = this._balance + amout;
+    }
+
+    private getBalance() {
+      return this._balance;
+    }
+
+    getHiddenMethod(){
+        return this.getBalance();
+    }
+
+  }
+
+  class StudentAccount extends BankAccount{
+    test(){
+        this.
+    }
+  }
+
+  const goridManuserAccount = new BankAccount(456, "Masud", 456);
+  goridManuserAccount.addAmount(500);
+
+  const myBalance = goridManuserAccount.getBalance();
+  console.log(myBalance);
+
+  //
+}
